f990f23a72b51c711aa20f5221e9b70132e99600
Septian Hadi Nugroho <septianhadinugroho4@gmail.com>
2025-10-22 11:45:44 +0700

    chore: add @types/leaflet-geosearch dependency and update Tailwind CSS config for TypeScript files

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,10 +3,9 @@ import type { Config } from "tailwindcss";
 const config: Config = {
     darkMode: "class",
     content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+	'./app/**/*.{ts,tsx}',
+	'./components/**/*.{ts,tsx}',
+	],
   theme: {
   	extend: {
   		colors: {
